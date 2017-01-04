833cac1e27763ca93919dd90fdba4a736f2a1cc9
alavi <alavi_hassan@yahoo.com>
2017-01-04 15:17:52 +0330

    I was writing the intro function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Padbot.V.4
+A PHP telegram bot for discourse platform.
+It uses the discourse's api and webhook.
--- bot.php
+++ bot.php
@@ -16,24 +16,86 @@ function makeCurl($method,$datas=[])    //macke and receive request to bot
     curl_close ($ch);
     return $server_output;
 }
+
+$db;                            //global database connect
+$level;                         //user level
+$user_id;                       //user unique user id.find in main function in each update
+$text;                          //text that user sent.sometimes the callback data of inline keyboard
+$username;                      //user telegram username
+$message_id;                    //message_id of button that user pressed
+$question;                      //the question should be asked from user
+$user_firstname;                //user first name;
+$last_updated_id = 0;           //should be removed
+function mysqlConnect($i)       //function for connection and disconnection to database
+{
+    global $db;
+    if ($i == 1)
+        $db=mysqli_connect("localhost","root","root","bot");
+    elseif ($i == 0)
+        echo mysqli_close($db);
+}
+
+function levelFinder()          //find user's level and return it
+{
+    global $user_id;
+    global $level;
+    global $db;
+    mysqlConnect(1);
+    $b = 0;
+    $result = mysqli_query($db,"SELECT * FROM Bot WHERE user_id={$user_id}");
+    while($row = mysqli_fetch_array($result))
+    {
+        if($row['level'])
+        {
+            $level = $row['level'];
+            $b = 1;
+        }
+    }
+    if($b == 0)
+        $level = 0;
+    mysqlConnect(0);
+}
+
+function intro()                //send user the introduction and add him to database and create his own database
+{
+    global $user_id;
+    //TODO complete this function
+}
+
 function main()
 {
     global $level;
-    global $chat_id;
+    global $user_id;
     global $text;
     global $username;
+    global $user_firstname;
     global $message_id;
-    $update = json_decode(file_get_contents("php://input"));
-    if ($update->callback_query) {
-        makeCurl("answerCallbackQuery", ["callback_query_id" => $update->callback_query->id]);
-        $text = $update->callback_query->data;
-        $chat_id = $update->callback_query->from->id;
-        $username = $update->callback_query->from->first_name;
-        $message_id = $update->callback_query->message->message_id;
-    } else {
-        $text = $update->message->text;
-        $chat_id = $update->message->chat->id;
-        $username = $update->message->from->first_name;
-    }
+    global $last_updated_id;
+    global $db;
+//    $update = json_decode(file_get_contents("php://input"));          //should not be comment
+    $updates = json_decode(makeCurl("getUpdates",["offset"=>($last_updated_id+1)]));        //should be removed
+    if($updates->ok == true && count($updates->result) > 0) {               //should be removed
+        foreach ($updates->result as $update) {                             //should be removed
+            if ($update->callback_query) {
+                makeCurl("answerCallbackQuery", ["callback_query_id" => $update->callback_query->id]);
+                $text = $update->callback_query->data;
+                $user_id = $update->callback_query->from->id;
+                $user_firstname = $update->callback_query->from->first_name;
+                $username = $update->callback_query->from->username;
+                $message_id = $update->callback_query->message->message_id;
+            } else {
+                $text = $update->message->text;
+                $user_id = $update->message->chat->id;
+                $username = $update->message->from->username;
+                $user_firstname = $update->message->from->first_name;
+            }
+            levelFinder();
+            if ($level == 0)
+                intro();
+            $last_updated_id = $update->update_id;              //should be removed
+        }           //should be removed
+    }               //should be removed
 }
-main();
+while(1) {
+    main();
+}
