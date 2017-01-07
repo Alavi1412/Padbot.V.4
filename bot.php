15c04941c13a22217a7f6bb8f60f6ef396c1fbb5
alavi <alavi_hassan@yahoo.com>
2017-01-07 19:51:24 +0330

    first step user function

--- bot.php
+++ bot.php
@@ -25,6 +25,7 @@ $username;                      //user telegram username
 $message_id;                    //message_id of button that user pressed
 $question;                      //the question should be asked from user
 $user_firstname;                //user first name;
+$locale;                        //user language
 $last_updated_id = 0;           //should be removed
 function mysqlConnect($i)       //function for connection and disconnection to database
 {
@@ -42,24 +43,57 @@ function levelFinder()          //find user's level and return it
     global $db;
     mysqlConnect(1);
     $b = 0;
-    $result = mysqli_query($db,"SELECT * FROM Bot WHERE user_id={$user_id}");
+    $result = mysqli_query($db,"SELECT * FROM bot.users WHERE user_id={$user_id}");
     while($row = mysqli_fetch_array($result))
     {
-        if($row['level'])
+        if($row['current_level'])
         {
-            $level = $row['level'];
+            $level = $row['current_level'];
+            $locale = $row['locale'];
             $b = 1;
         }
     }
     if($b == 0)
-        $level = 0;
+        $level = "Begin";
     mysqlConnect(0);
 }
 
 function intro()                //send User the introduction and add him to database and create his own database
 {
     global $user_id;
-    //TODO complete this function
+    global $db;
+    global $user_firstname;
+    global $username;
+    mysqlConnect(1);
+    makeCurl("sendPhoto",["chat_id" => $user_id, "photo" => "https://meta-s3-cdn.global.ssl.fastly.net/original/3X/c/a/ca61ea9e8fa9b8046cd59c524f7e0f76c912211f.png", "reply_markup" => json_encode([
+        "inline_keyboard" =>[
+            [
+                ["text" => "فارسی" , "callback_data" => "P3R$1an"],["text" => "English", "callback_data" => "3ngL1$1h"]
+            ]
+        ]
+    ])]);
+    mysqli_query($db, "INSERT INTO bot.users (user_id, user_first_name, current_level, username) VALUES ({$user_id}, \"{$user_firstname}\", 'intro_showed', \"{$username}\")");
+    mysqlConnect(0);
+}
+
+function firstStep()                //the first step for user after click on locale. table for each user created here
+{
+    //TODO this function should be completed
+    global $user_id;
+    global $text;
+    global $db;
+    mysqlConnect(1);
+    if( $text == "P3R$1an")
+    {
+        mysqli_query($db, "UPDATE bot.users SET loacle = 'farsi', current_level = 'firstStep' WHERE user_id = {$user_id}");
+        makeCurl("sendMessage", ["chat_id" => $user_id, "text" => "به پادپرس بات خوش آمدید. ایمیل خودتون رو وارد کنید و اگر یوزر پادپرس هستید ایمیل پادپرست رو وارد کن."]);
+    }elseif ($text == "3ngL1$1h")
+    {
+        mysqli_query($db, "UPDATE bot.users SET loacle = 'english', current_level = 'firstStep' WHERE user_id = {$user_id}");
+        makeCurl("sendMessage", ["chat_id" => $user_id, "text" => "Enter your email:"]);
+    }
+    mysqli_query($db, "CREATE TABLE bot.user{$user_id} ( content LONGTEXT CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL , score INT NOT NULL DEFAULT '0' , question_number INT NOT NULL , group_of_answer_master_key INT NOT NULL , answer_id INT NOT NULL )");
+    mysqlConnect(0);
 }
 
 function main()
@@ -90,8 +124,10 @@ function main()
                 $user_firstname = $update->message->from->first_name;
             }
             levelFinder();
-            if ($level == 0)
+            if ($level == "Begin")
                 intro();
+            elseif ($level == "intro_showed")
+                firstStep();
             $last_updated_id = $update->update_id;              //should be removed
         }           //should be removed
     }               //should be removed
